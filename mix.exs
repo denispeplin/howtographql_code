8f05e04aca4a310e79003d2f06e44515e8abc54e
denispeplin <denis.peplin@gmail.com>
2021-11-20 18:53:14 +0300

    Add Absinthe dependency.

--- mix.exs
+++ mix.exs
@@ -42,6 +42,8 @@ defmodule Community.MixProject do
       {:telemetry_metrics, "~> 0.6"},
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
+      {:absinthe, "~> 1.6"},
+      {:absinthe_plug, "~> 1.5"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"}
     ]
